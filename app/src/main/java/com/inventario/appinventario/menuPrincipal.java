f07ef863c03279f1b71fed3a4ff0444aa2ae2bd3
JesusGoyenecheSena <jagoyeneche15@soy.sena.edu.co>
2023-09-13 21:04:41 -0500

    modificacion botón registrarse

--- app/src/main/java/com/inventario/appinventario/menuPrincipal.java
+++ app/src/main/java/com/inventario/appinventario/menuPrincipal.java
@@ -24,4 +24,10 @@ public class menuPrincipal extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void irRegistro(View view){
+        Intent i = new Intent(this, registro.class);
+        startActivity(i);
+    }
+
+
 }
